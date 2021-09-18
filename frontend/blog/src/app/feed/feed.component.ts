347b04399e5cae7931ec4dd07192cc42507be058
Ricardo Gomes <sleiph@gmail.com>
2021-09-17 21:41:46 -0300

    deu tudo errado denovo t.t

--- frontend/blog/src/app/feed/feed.component.ts
+++ frontend/blog/src/app/feed/feed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../service/post.service';
+import { Post } from '../model/Post';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  listPost: Post[] = new Array(99);
+  post: Post = new Post;
+  nome: string = "";
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.findPosts()
+  }
+
+  findPosts() {
+    this.postService.getPosts().subscribe(
+      (data: Post[]) => {
+        this.listPost = data;
+      }
+    )
+  }
+
+  cadastrarMensagem() {
+    this.postService.postMensagem(this.post).subscribe((data: Post) => {
+      this.post = data
+      location.assign('/feed')
+    })
   }
 
 }
